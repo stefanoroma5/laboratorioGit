a0f3fbd987e1b52abbce6035bb72864046e2306b
Stefano Romanini <stefano01.romanini@edu.unife.it>
2022-03-24 16:09:51 +0100

    Inserite nuove operazioni di stampa

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main (String[] args) {
         System.out.println("Prima stampa");
+        System.out.println("Seconda stampa");
+        System.out.println("Terza stampa");
     }
 }
