23ae4320b934a30af46ac13daf62fbf0fb635cca
Stefano Romanini <stefano01.romanini@edu.unife.it>
2022-03-24 16:44:08 +0100

    Seconda print in master

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Terza stampa");
         
         System.out.println("Inserito nel branch test");
+        System.out.println("Seconda print inserita nel branch master");
     }
 }
